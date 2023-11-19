c566cfed394232b7cbdeb7f264a5595c767fd68e
David Goldstein <goldstein.g.david@gmail.com>
2023-11-19 10:57:33 -0500

    Some very basic data processing to aggregate observed volume and event count

--- consume.py
+++ consume.py
@@ -1,3 +1,6 @@
+import pprint
+import traceback
+
 from kafka import KafkaConsumer
 from json import loads
 
@@ -11,6 +14,39 @@ consumer = KafkaConsumer(
 )
 
 
+class TickerStat:
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.counts = 0
+        self.volume = 0.0
+
+    def __str__(self):
+        return f"{self.ticker}: {self.counts} occurences; {self.volume} volume"
+
+    def __repr__(self):
+        return f"{self.ticker}: {self.counts} occurences; {self.volume} volume"
+
+
+def update_ticker_stats(ticker_data):
+    try:
+        for t in ticker_data["data"]:
+            if t["s"] not in totals:
+                totals[t["s"]] = TickerStat(t["s"])
+
+            ticker_stat = totals[t["s"]]
+            ticker_stat.counts += 1
+            ticker_stat.volume += t["v"]
+    except KeyError:
+        print(traceback.format_exc())
+        return
+
+
+pp = pprint.PrettyPrinter(indent=4)
+totals = {}
+
+
 if __name__ == "__main__":
     for m in consumer:
-        print(m.value)
+        ticker_data = loads(m.value)
+        update_ticker_stats(ticker_data)
+        pp.pprint(totals)
